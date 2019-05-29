3740b64d0d8098800f8762372adb4073b19e8082
André Maree <andrem@kss.co.za>
2019-05-29 22:52:49 +0200

    Rename OW functions in line with 1-Wire standard
    Cleanup enumerations,
    Define macros for individual operations wait periods
    Add semaphore to protect DS2482 exclusive access

--- ds2482.h
+++ ds2482.h
@@ -31,11 +31,13 @@
 
 // ############################################# Macros ############################################
 
-// Device base address
-#define	configHAL_I2C_DS2482_ADDR			0x18
+#define	configHAL_I2C_DS2482_ADDR			0x18		// Device base address
 
-// misc constants
-#define POLL_LIMIT  						200
+#define	owDELAY_WB							1			// 0=Yield >0=mS Delay
+// 0=NoWait 1=Yield >1=Delay(n-1)
+#define	owDELAY_ST							2			// Search Triplet
+#define	owDELAY_RST							2			// Bus Reset
+#define	owDELAY_TB							1			// Touch Bit
 
 // DS2482 commands
 #define CMD_DRST   							0xF0		// Device Reset
@@ -66,14 +68,6 @@
 
 // ######################################## Enumerations ###########################################
 
-enum {													// DS2482 Register numbers
-	ds2482REG_STAT = 0,
-	ds2482REG_DATA,
-	ds2482REG_CHAN,
-	ds2482REG_CONF,
-	ds2482REG_NUM,
-} ;
-
 enum {													// Supported 1W families & devices
 	idxOWFAMILY_01,
 #if		(configBUILD_WITH_DS18X20 == 1)
@@ -82,18 +76,8 @@ enum {													// Supported 1W families & devices
 	idxOWFAMILY_NUM,
 } ;
 
-enum {													// Channels
-	sd2482CHAN_0 = 0,
-	sd2482CHAN_1,
-	sd2482CHAN_2,
-	sd2482CHAN_3,
-	sd2482CHAN_4,
-	sd2482CHAN_5,
-	sd2482CHAN_6,
-	sd2482CHAN_7,
-	sd2482CHAN_NUM,
-	sd2482CHAN_ANY	= 0xFF,
-} ;
+enum { ds2482REG_STAT, ds2482REG_DATA, ds2482REG_CHAN, ds2482REG_CONF, ds2482REG_NUM } ;
+enum { sd2482CHAN_0, sd2482CHAN_1, sd2482CHAN_2, sd2482CHAN_3, sd2482CHAN_4, sd2482CHAN_5, sd2482CHAN_6, sd2482CHAN_7, sd2482CHAN_NUM } ;
 
 // ######################################### Structures ############################################
 
@@ -131,28 +115,48 @@ typedef union {
 	uint8_t			RegX[4] ;
 } ds2482_regs_t ;
 
-typedef struct {
+typedef struct {										// DS2482 I2C <> 1Wire bridge
 	halI2Cdev_t		sI2Cdev ;
 	ds2482_regs_t	Regs ;
 	ow_rom_t		ROM ;
+	SemaphoreHandle_t	Mux ;
 	int32_t 		LastDiscrepancy ;
 	int32_t 		LastFamilyDiscrepancy ;
 	uint8_t 		crc8 ;
 	uint8_t			CurChan			: 3 ;
 	uint8_t			RegPntr			: 2 ;
 	uint8_t 		LastDeviceFlag	: 1 ;
-} DS2482_t ;
+} ds2482_t ;
 
-extern DS2482_t	sDS2482 ;
+// #################################### Public Data structures #####################################
+
 extern uint8_t	FamilyCount[], ChannelCount[] ;
+extern ds2482_t	sDS2482 ;
 
 // ###################################### Private functions ########################################
 
-void	halDS2482_PrintROM(ow_rom_t * psOW_ROM) ;
-uint8_t	halDS2482_Report(DS2482_t * psDS2482) ;
+int32_t OWReset(void) ;
 
-int32_t	halDS2482_ScanAll(void) ;
-int32_t	halDS2482_ScanButton(void) ;
+uint8_t	OWCheckCRC(uint8_t * buf, uint8_t buflen) ;
+void	OWAddress(uint8_t nAddrMethod) ;
+int32_t	OWWriteByte(uint8_t sendbyte) ;
+int32_t OWWriteBytePower(int32_t sendbyte) ;
+int32_t	OWWriteByteWait(uint8_t sendbyte) ;
+void	OWBlock(uint8_t * tran_buf, int32_t tran_len) ;
+int32_t OWLevel(int32_t new_level) ;
 
-int32_t	halDS2482_Diagnostics(void) ;
-int32_t	halDS2482_Identify(uint8_t chanI2C, uint8_t addrI2C) ;
+void	ds2482PrintROM(ow_rom_t * psOW_ROM) ;
+uint8_t	ds2482Report(void) ;
+int32_t ds2482ChannelSelect(uint8_t Chan) ;
+
+int32_t	ds2482ScanChannelAll(void) ;
+int32_t	ds2482ScanFam01(void) ;
+
+float	DS2482_ReadFam10(int32_t Idx) ;
+int32_t	DS2482_ScanFam10All(ep_work_t * psEpWork) ;
+
+int32_t	ds2482ScanAndCall(uint8_t Family, int32_t (* Handler)(int32_t)) ;
+int32_t	ds2482Diagnostics(void) ;
+int32_t	ds2482Identify(uint8_t chanI2C, uint8_t addrI2C) ;
+int32_t	ds2482Config(void) ;
+void	ds2482Tests(void) ;
