90188f8e6dd3bca1601b9e7374ddbbb59bef5fb5
André Maree <ammaree@users.noreply.github.com>
2020-11-27 17:25:11 +0200

    Refactor for clarity & simplicity

--- onewire_platform.c
+++ onewire_platform.c
@@ -194,15 +194,14 @@ int32_t	OWPlatformScanner(uint8_t Family, int (* Handler)(flagmask_t, onewire_t 
 			iRV = OWCheckCRC(psOW->ROM.HexChars, sizeof(ow_rom_t)) ;
 			IF_myASSERT(debugRESULT, iRV == 1) ;
 			iRV = Handler((flagmask_t) uCount, psOW) ;
-			if (iRV < erSUCCESS) {
+			if (iRV < erSUCCESS)
 				break ;
-			}
-			if (iRV > 0) {
+			if (iRV > 0)
 				++uCount ;
 			}
 			iRV = OWNext(psOW, false) ;						// try to find next device (if any)
 		}
-		if (iRV < erSUCCESS) {
+		if (iRV < erSUCCESS)
 			break ;
 		}
 	}
@@ -214,7 +213,7 @@ int32_t OWPlatformEndpoints(struct ep_work_s * psEpWork) {
 	int32_t iRV = erFAILURE;
 	switch(psEpWork->uri) {
 #if		(halHAS_DS18X20 > 0)
-	case URI_DS18X20: iRV = ds18x20ReadConvertAll(NULL) ;	break ;
+	case URI_DS18X20:	iRV = ds18x20ReadConvertAll(NULL) ;		break ;
 #endif
 
 #if		(halHAS_DS1990X > 0)
@@ -259,22 +258,19 @@ int32_t	OWPlatformConfig(void) {
 
 		// enumerate any/all physical devices (possibly) (permanently) attached to individual channel(s)
 		onewire_t	sOW ;
-		if ((iRV = OWPlatformScanner(0, OWPlatformCB_Count, &sOW)) > 0) {
+		if ((iRV = OWPlatformScanner(0, OWPlatformCB_Count, &sOW)) > 0)
 			OWNumDev += iRV ;
-		}
+
 #if		(halHAS_DS1990X > 0)
-		if (Family01Count) {
-			IF_SL_DBG(debugTRACK, "DS1990x found %d devices", Family01Count) ;
-		}
-		iRV = ds1990xConfig(URI_DS1990X) ;
+		IF_SL_DBG(debugTRACK && Family01Count, "DS1990x found %d devices", Family01Count) ;
+		iRV = ds1990xConfig(URI_DS1990X) ;				// cannot enumerate, simple config
 		IF_SYSTIMER_INIT(debugTIMING, systimerDS1990, systimerTICKS, "DS1990", myMS_TO_TICKS(10), myMS_TO_TICKS(1000)) ;
 #endif
 
 #if		(halHAS_DS18X20 > 0)
-		if (Fam10_28Count) {
-			IF_SL_DBG(debugTRACK, "DS18x20 found %d devices", Fam10_28Count) ;
-			iRV = ds18x20Enumerate(URI_DS18X20) ;
-		}
+		IF_SL_DBG(debugTRACK && Fam10_28Count, "DS18x20 found %d devices", Fam10_28Count) ;
+		if (Fam10_28Count)
+			iRV = ds18x20Enumerate(URI_DS18X20) ;		// enumerate & config individually
 		IF_SYSTIMER_INIT(debugTIMING, systimerDS1820A, systimerTICKS, "DS1820A", myMS_TO_TICKS(10), myMS_TO_TICKS(1000)) ;
 		IF_SYSTIMER_INIT(debugTIMING, systimerDS1820B, systimerTICKS, "DS1820B", myMS_TO_TICKS(1), myMS_TO_TICKS(10)) ;
 #endif
