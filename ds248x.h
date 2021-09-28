d3b1e47eb1d5e4747b4616f069102d2d71c8fd07
André Maree <ammaree@users.noreply.github.com>
2021-09-28 21:32:33 +0200

    Move variables to correct module

--- ds248x.h
+++ ds248x.h
@@ -135,6 +135,8 @@ typedef struct __attribute__((packed)) ds248x_t {		// DS248X I2C <> 1Wire bridge
 
 // #################################### Public Data structures #####################################
 
+extern uint8_t ds248xCount;
+extern ds248x_t * psaDS248X;
 
 // ################################ DS248X I2C Read/Write support ##################################
 
