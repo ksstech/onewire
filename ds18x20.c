12a2e24b4b12e27c0c3bc6c53f566e2f9caf1b6c
André Maree <ammaree@users.noreply.github.com>
2021-10-29 11:59:13 +0200

    Use new macros

--- ds18x20.c
+++ ds18x20.c
@@ -188,8 +188,7 @@ int	ds18x20SetResolution(ds18x20_t * psDS18X20, int Res) {
 		psDS18X20->Res = Res ;
 		return 1 ;										// changed, must write
 	}
-	SET_ERRINFO("Invalid Family/Resolution") ;
-	return erSCRIPT_INV_VALUE ;
+	ERR_RETURN("Invalid Family/Resolution", erSCRIPT_INV_VALUE);
 }
 
 int	ds18x20SetAlarms(ds18x20_t * psDS18X20, int Lo, int Hi) {
@@ -200,12 +199,12 @@ int	ds18x20SetAlarms(ds18x20_t * psDS18X20, int Lo, int Hi) {
 		psDS18X20->Thi = Hi ;
 		return 1 ;										// changed, must write
 	}
-	SET_ERRINFO("Invalid Lo/Hi alarm limits") ;
-	return erSCRIPT_INV_VALUE ;
+	ERR_RETURN("Invalid Lo/Hi alarm limits", erSCRIPT_INV_VALUE);
 }
 
-	if (psaDS18X20 == NULL) { SET_ERRINFO("No DS18x20 enumerated"); return erSCRIPT_INV_OPERATION; }
 int	ds18x20ConfigMode (struct rule_t * psR, int Xcur, int Xmax) {
+	if (psaDS18X20 == NULL)
+		ERR_RETURN("No DS18x20 enumerated", erSCRIPT_INV_OPERATION);
 	// support syntax mode /ow/ds18x20 idx lo hi res [1=persist]
 	int iRV = erFAILURE, iRVx = erFAILURE;
 	uint8_t	AI = psR->ActIdx ;
