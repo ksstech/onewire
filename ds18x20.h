116d9b516e68c3b87af969cb94e15edab8a1daed
André Maree <ammaree@users.noreply.github.com>
2022-06-07 19:47:31 +0200

    Change integers from long to short typedef

--- ds18x20.h
+++ ds18x20.h
@@ -19,10 +19,10 @@
 // See http://www.catb.org/esr/structure-packing/
 // Also http://c0x.coding-guidelines.com/6.7.2.1.html
 
-typedef	struct __attribute__((packed)) fam10 { uint8_t Res0, Res1, Remain, Count ; } fam10 ;
+typedef	struct __attribute__((packed)) fam10 { u8_t Res0, Res1, Remain, Count ; } fam10 ;
 DUMB_STATIC_ASSERT(sizeof(fam10) == 4) ;
 
-typedef	struct __attribute__((packed)) fam28 { uint8_t Conf, Res1, Res2, Res3 ; } fam28 ;
+typedef	struct __attribute__((packed)) fam28 { u8_t Conf, Res1, Res2, Res3 ; } fam28 ;
 DUMB_STATIC_ASSERT(sizeof(fam28) == 4) ;
 
 typedef struct __attribute__((packed)) {				// DS1820/S20/B20 9/12 bit Temp sensors
@@ -30,25 +30,25 @@ typedef struct __attribute__((packed)) {				// DS1820/S20/B20 9/12 bit Temp sens
 	epw_t	sEWx ;										// size = 32
 	union {												// Scratchpad
 		struct __attribute__((packed)) {
-			uint8_t	Tlsb, Tmsb ;						// last RAM sample
-			uint8_t	Thi, Tlo ;							// Integer portion of Lo & Hi alarm thresholds
+			u8_t	Tlsb, Tmsb ;						// last RAM sample
+			u8_t	Thi, Tlo ;							// Integer portion of Lo & Hi alarm thresholds
 			union {
 				fam10 fam10 ;
 				fam28 fam28 ;
 			} ;
-			uint8_t	CRC ;								// calculated CRC of previous 8 bytes
+			u8_t	CRC ;								// calculated CRC of previous 8 bytes
 		} ;
-		uint8_t	RegX[9] ;
+		u8_t	RegX[9] ;
 	} ;
-	uint8_t	Idx		: 3 ;								// Endpoint index (0->7) of this specific device
-	uint8_t	Res		: 2 ;								// Resolution 0=9b 1=10b 2=11b 3=12b
-	uint8_t	SBits	: 3 ;
+	u8_t	Idx		: 3 ;								// Endpoint index (0->7) of this specific device
+	u8_t	Res		: 2 ;								// Resolution 0=9b 1=10b 2=11b 3=12b
+	u8_t	SBits	: 3 ;
 } ds18x20_t ;
 DUMB_STATIC_ASSERT(sizeof(ds18x20_t) == 54) ;
 
 // ###################################### Public variables #########################################
 
-extern uint8_t Fam10Count, Fam28Count;
+extern u8_t Fam10Count, Fam28Count;
 
 // ###################################### Public functions #########################################
 
