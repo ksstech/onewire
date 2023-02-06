612463f2f05aea1d41562a636888b00e58a398e4
André Maree <ammaree@users.noreply.github.com>
2023-02-06 19:24:02 +0200

    Initial RMT peripheral support

---
+++ owb_rmt.c
@@ -0,0 +1,71 @@
+/*
+ * ds248x.c
+ * Copyright (c) 2020-22 Andre M. Maree / KSS Technologies (Pty) Ltd.
+ */
+
+#include "main.h"
+
+#if (halRMT_1W > 0)
+#include "onewire_platform.h"
+
+#include "FreeRTOS_Support.h"
+#include "printfx.h"
+#include "syslog.h"
+#include "systiming.h"								// timing debugging
+#include "x_errors_events.h"
+#include "x_string_general.h"
+
+// ###################################### General macros ###########################################
+
+#define	debugFLAG					0xF000
+
+#define	debugTIMING					(debugFLAG_GLOBAL & debugFLAG & 0x1000)
+#define	debugTRACK					(debugFLAG_GLOBAL & debugFLAG & 0x2000)
+#define	debugPARAM					(debugFLAG_GLOBAL & debugFLAG & 0x4000)
+#define	debugRESULT					(debugFLAG_GLOBAL & debugFLAG & 0x8000)
+
+// ######################################## Build macros ###########################################
+
+
+// ##################################### Local structures ##########################################
+
+
+// ###################################### Local variables ##########################################
+
+
+// ##################################### Global variables ##########################################
+
+u8_t rmtCount	= 0;
+owb_rmt_t * psaRMT = NULL;
+
+// ##################################### Forward declarations ######################################
+
+
+// #################################### DS248x debug/reporting #####################################
+
+void ds248xReport(owb_rmt_t * psRMT) {}
+
+void ds248xReportAll(void) {}
+
+// ################################## DS248x-x00 1-Wire functions ##################################
+
+int	rmtOWReset(owb_rmt_t * psRMT) { esp_err_t onewire_bus_reset(onewire_bus_handle_t handle); }
+
+int	rmtOWSpeed(owb_rmt_t * psRMT, bool speed) {}
+
+int	rmtOWLevel(owb_rmt_t * psRMT, bool level) {}
+
+bool rmtOWWriteBit(owb_rmt_t * psRMT, bool Bit) { esp_err_t onewire_bus_write_bit(onewire_bus_handle_t handle, uint8_t tx_bit); }
+
+bool rmtOWReadBit(owb_rmt_t * psRMT, bool Bit) { esp_err_t onewire_bus_read_bit(onewire_bus_handle_t handle, uint8_t *rx_bit); }
+
+u8_t rmtOWWriteByte(owb_rmt_t * psRMT, u8_t Byte) {}
+
+u8_t rmtOWWriteBytes(owb_rmt_t * psRMT, u8_t * Bytes) {
+	esp_err_t onewire_bus_read_bytes(onewire_bus_handle_t handle, uint8_t *rx_data, size_t rx_data_size)
+}
+
+u8_t rmtOWReadByte(owb_rmt_t * psRMT) {}
+
+u8_t rmtOWSearchTriplet(owb_rmt_t * psRMT, u8_t u8Dir) {}
+#endif
---
+++ priv/esp_rmt.h
@@ -0,0 +1,41 @@
+/*
+ * esp_rmt.h
+ * Copyright 2023 Andre M. Maree/KSS Technologies (Pty) Ltd.
+ */
+
+#pragma once
+
+// ######################################## Enumerations ###########################################
+
+
+// ######################################### Structures ############################################
+
+typedef struct owb_rmt_t {
+	SemaphoreHandle_t mux;
+	StaticTimer_t ts;
+	#if (halHAS_DS18X20 > 0)
+	TimerHandle_t th;
+	#endif
+} owb_rmt_t;
+
+// #################################### Public Data structures #####################################
+
+extern owb_rmt_t * psaRMT;
+
+// ###################################### Device debug support #####################################
+
+int	owb_rmtReportRegister(owb_rmt_t * psDS248X, int Reg) ;
+void owb_rmtReport(owb_rmt_t * psDS248X) ;
+void owb_rmtReportAll(void) ;
+int	owb_rmtBusSelect(owb_rmt_t * psDS248X, u8_t Chan) ;
+void owb_rmtBusRelease(owb_rmt_t * psDS248X) ;
+
+// #################################### 1-Wire support functions ###################################
+
+int	rmtOWReset(owb_rmt_t * psDS248X) ;
+int	rmtOWSpeed(owb_rmt_t * psDS248X, bool speed) ;
+int	rmtOWLevel(owb_rmt_t * psDS248X, bool level) ;
+bool rmtOWTouchBit(owb_rmt_t * psDS248X, bool bit) ;
+u8_t rmtOWWriteByte(owb_rmt_t * psDS248X, u8_t sendbyte) ;
+u8_t rmtOWReadByte(owb_rmt_t * psDS248X);
+u8_t owb_rmtOWSearchTriplet(owb_rmt_t * psDS248X, u8_t u8Dir) ;
