979e522be20023fee350bdcfe6cce41af66a3808
André Maree <ammaree@users.noreply.github.com>
2023-07-04 01:03:46 +0200

    Update onewire.c

--- onewire.c
+++ onewire.c
@@ -11,7 +11,7 @@
 #include "syslog.h"
 #include "x_errors_events.h"
 
-#define	debugFLAG					0xD000
+#define	debugFLAG					0xF000
 
 #define	debugTIMING					(debugFLAG_GLOBAL & debugFLAG & 0x1000)
 #define	debugTRACK					(debugFLAG_GLOBAL & debugFLAG & 0x2000)
