629c6909a1adec51fadef64321dcf547b7fb6e04
André Maree <ammaree@users.noreply.github.com>
2023-08-14 00:27:13 +0200

    Add locking support to report_t functionality

--- onewire_platform.c
+++ onewire_platform.c
@@ -105,6 +105,7 @@ void OWP_BusRelease(owdi_t * psOW) { ds248xBusRelease(&psaDS248X[psOW->DevNum]);
  */
 int	OWP_PrintROM_CB(report_t * psR, ow_rom_t * psOW_ROM) {
 	int iRV = 0;
+	WPRINTFX_LOCK(&psR->pcBuf, &psR->Size);
 	if (psR->sFM.bRT)
 		iRV += wprintfx(psR, "%!.R: ", RunTime);
 	if (psR->sFM.bTskNum)
@@ -112,6 +113,7 @@ int	OWP_PrintROM_CB(report_t * psR, ow_rom_t * psOW_ROM) {
 	iRV += wprintfx(psR, "%02X/%M/%02X", psOW_ROM->HexChars[owFAMILY], &psOW_ROM->HexChars[owAD0], psOW_ROM->HexChars[owCRC]);
 	if (psR->sFM.bNL)
 		iRV += wprintfx(psR, strCRLF);
+	WPRINTFX_UNLOCK(&psR->pcBuf, &psR->Size);
 	return iRV;
 }
 
@@ -120,9 +122,11 @@ int	OWP_Print1W_CB(report_t * psR, owdi_t * psOW) {
 	psR->sFM.u32Val &= ~mfbNL;
 	int iRV = OWP_PrintROM_CB(psR, &psOW->ROM);
 	psR->sFM.bNL = ((fm_t) U32).bNL;
+	WPRINTFX_LOCK(&psR->pcBuf, &psR->Size);
 	iRV += wprintfx(psR, "  Log=%d  Dev=%d  Phy=%d  PSU=%d", OWP_BusP2L(psOW), psOW->DevNum, psOW->PhyBus, psOW->PSU);
 	if (psR->sFM.bNL)
 		iRV += wprintfx(psR, strCRLF);
+	WPRINTFX_UNLOCK(&psR->pcBuf, &psR->Size);
 	return iRV;
 }
 
@@ -134,6 +138,7 @@ int	OWP_PrintChan_CB(report_t * psR, owbi_t * psCI) {
 		iRV += OWP_PrintROM_CB(psR, &psCI->LastROM);
 		psR->sFM.u32Val |= (U32 & (mfbRT|mfbNL|mfbCOUNT));
 	}
+	WPRINTFX_LOCK(&psR->pcBuf, &psR->Size);
 	iRV += wprintfx(psR, " OW#%d ", psR->sFM.uCount);
 	if (psCI->LastRead)
 		iRV += wprintfx(psR, "%R ", xTimeMakeTimestamp(psCI->LastRead, 0));
@@ -141,6 +146,7 @@ int	OWP_PrintChan_CB(report_t * psR, owbi_t * psCI) {
 		iRV += wprintfx(psR, "DS18B=%d DS18S=%d", psCI->ds18b20, psCI->ds18s20);
 	if (psR->sFM.bNL)
 		iRV += wprintfx(psR, strCRLF);
+	WPRINTFX_UNLOCK(&psR->pcBuf, &psR->Size);
 	return iRV;
 }
 
