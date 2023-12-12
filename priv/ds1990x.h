b90a1ba13eee4b8a15641e54411139c718d3f8d6
André Maree <ammaree@users.noreply.github.com>
2023-12-12 18:53:08 +0200

    Update ds1990x.h

--- priv/ds1990x.h
+++ priv/ds1990x.h
@@ -1,9 +1,12 @@
 /*
- * ds1990x.h
- * Copyright (c) 2018-22 Andre M. Maree/KSS Technologies (Pty) Ltd.
+ * ds1990x.h - Copyright (c) 2018-23 Andre M. Maree/KSS Technologies (Pty) Ltd.
  */
 
 #pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 // ############################################# Macros ############################################
 
@@ -13,7 +16,6 @@
 
 // ######################################### Structures ############################################
 
-struct epw_t;
 
 // ###################################### Public variables #########################################
 
@@ -21,4 +23,10 @@ struct epw_t;
 // ###################################### Public functions #########################################
 
 void ds1990xConfig(void);
+struct epw_t;
 int	ds1990Sense(struct epw_t * psEWP);
+
+#ifdef __cplusplus
+}
+#endif
+
