e47162f2d380a5023e6cd38613d0b8257590a949
André Maree <ammaree@users.noreply.github.com>
2025-01-16 17:07:05 +0200

    Update copyright notice for 2025

--- onewire_platform.c
+++ onewire_platform.c
@@ -1,4 +1,4 @@
-// onewire_platform.c - Copyright (c) 2020-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
+// onewire_platform.c - Copyright (c) 2020-25 Andre M. Maree / KSS Technologies (Pty) Ltd.
 
 #include "hal_platform.h"
 #include "hal_flash.h"
