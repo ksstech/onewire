9741d3af89069ed8625c90118cebf0e4ac8e643d
André Maree <ammaree@users.noreply.github.com>
2025-01-16 17:06:51 +0200

    Update copyright notice for 2025

--- ds248x.c
+++ ds248x.c
@@ -1,4 +1,4 @@
-// ds248x.c - Copyright (c) 2020-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
+// ds248x.c - Copyright (c) 2020-25 Andre M. Maree / KSS Technologies (Pty) Ltd.
 
 #include "hal_platform.h"
 #include "hal_i2c_common.h"
